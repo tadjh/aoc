ad5b6c5d2fceaa1bedf878599474ea7a2414b6d2
tadjh <tadjh.brooks@gmail.com>
2022-12-02 21:55:46 -0800

    Day 3

--- src/3/a.ts
+++ src/3/a.ts
@@ -1,13 +1,30 @@
 // deno run --watch --allow-all a.ts
 
 export {};
-const input = await Deno.readTextFile("test.txt");
-const arr = input.split("");
+const input = await Deno.readTextFile("input.txt");
+const arr = input.split("\r\n").map((el) => el.split(""));
 
-let output = "";
+const letters = "abcdefghijklmnopqrstuvwxyz";
+const chars = letters.split("").concat(letters.toUpperCase().split(""));
+
+let score: { [key: string]: number } = {};
+
+for (let i = 0; i < chars.length; i++) {
+  score = { ...score, [chars[i]]: i + 1 };
+}
+
+let output = 0;
 
 for (let i = 0; i < arr.length; i++) {
-  output += arr[i];
+  const comp1 = arr[i].slice(0, Math.ceil(arr[i].length / 2));
+  const comp2 = arr[i].slice(Math.ceil(arr[i].length / 2));
+
+  for (let j = 0; j < comp1.length; j++) {
+    if (comp2.includes(comp1[j])) {
+      output += score[comp1[j]];
+      break;
+    }
+  }
 }
 
 console.log(output);
--- src/3/b.ts
+++ src/3/b.ts
@@ -1,13 +1,40 @@
-// deno run --watch --allow-all b.ts
+// deno run --watch --allow-all a.ts
+const letters = "abcdefghijklmnopqrstuvwxyz";
+const chars = letters.split("").concat(letters.toUpperCase().split(""));
 
+let score: { [key: string]: number } = {};
+
+for (let i = 0; i < chars.length; i++) {
+  score = { ...score, [chars[i]]: i + 1 };
+}
 export {};
-const input = await Deno.readTextFile("test.txt");
-const arr = input.split("");
+const input = await Deno.readTextFile("input.txt");
+const arr = input.split("\r\n").map((el) => el.split(""));
+let output = 0;
+const chunkSize = 3;
+for (let i = 0; i < arr.length; i += chunkSize) {
+  const [sack1, sack2, sack3] = arr.slice(i, i + chunkSize);
 
-let output = "";
+  const hashes: { [key: string]: boolean } = {};
 
-for (let i = 0; i < arr.length; i++) {
-  output += arr[i];
+  for (const item1 of sack1) {
+    if (!(item1 in hashes)) {
+      hashes[item1] = false;
+    }
+  }
+
+  for (const item2 of sack2) {
+    if (item2 in hashes) {
+      hashes[item2] = true;
+    }
+  }
+
+  for (const item3 of sack3) {
+    if (item3 in hashes && hashes[item3] === true) {
+      output += score[item3];
+      break;
+    }
+  }
 }
 
 console.log(output);
