f025279fc310a646a5ccd1c1d3e81f45aaf98bbe
tadjh <tadjh.brooks@gmail.com>
2022-12-03 19:18:50 -0800

    2015 day 3

--- src/2015/3/a.ts
+++ src/2015/3/a.ts
@@ -1,17 +1,57 @@
 // deno run --watch --allow-all a.ts
 
 export {};
-const input = await Deno.readTextFile("test.txt");
-const arr = input.split("") as string[];
+const input = await Deno.readTextFile("input.txt");
+const arr = input.split("\r\n").map((el) => el.split("")) as (
+  | "^"
+  | ">"
+  | "v"
+  | "<"
+)[][];
 
-let output = "";
+console.log(arr);
 
-const _lookup = {
-  "": 0,
+let output = 0;
+
+const move: { [key in "^" | ">" | "v" | "<"]: [number, number] } = {
+  "^": [0, -1],
+  ">": [1, 0],
+  v: [0, 1],
+  "<": [-1, 0],
 };
 
-for (let i = 0; i < arr.length; i++) {
-  output += arr[i];
-}
+const add = (
+  [a0, a1]: [number, number],
+  [b0, b1]: [number, number]
+): [number, number] => [a0 + b0, a1 + b1];
 
-console.log(output);
+for (let i = 0; i < arr.length; i++) {
+  const visited: { [key: string]: number } = {};
+
+  let visits = 1;
+
+  let position: [number, number] = [0, 0];
+
+  const [a, b] = position;
+
+  visited[a + "," + b] = 1;
+
+  for (let j = 0; j < arr[i].length; j++) {
+    position = add(position, move[arr[i][j]]);
+
+    const [c, d] = position;
+
+    if (!visited[c + "," + d]) {
+      visited[c + "," + d] = 1;
+      ++visits;
+      continue;
+    }
+
+    visited[c + "," + d] += 1;
+  }
+
+  // console.log(visited);
+  console.log(visits);
+
+  output += visits;
+}
--- src/2015/3/b.ts
+++ src/2015/3/b.ts
@@ -1,17 +1,63 @@
 // deno run --watch --allow-all a.ts
 
 export {};
-const input = await Deno.readTextFile("test.txt");
-const arr = input.split("") as string[];
+const input = await Deno.readTextFile("input.txt");
+const arr = input.split("") as ("^" | ">" | "v" | "<")[];
 
-let output = "";
+console.log(arr);
 
-const _lookup = {
-  "": 0,
+// let output = 0;
+
+const move: { [key in "^" | ">" | "v" | "<"]: [number, number] } = {
+  "^": [0, -1],
+  ">": [1, 0],
+  v: [0, 1],
+  "<": [-1, 0],
 };
 
+const add = (
+  [a0, a1]: [number, number],
+  [b0, b1]: [number, number]
+): [number, number] => [a0 + b0, a1 + b1];
+
+const visited: { [key: string]: number } = {};
+
+let visits = 1;
+
+let position1: [number, number] = [0, 0];
+let position2: [number, number] = [0, 0];
+
+const [a, b] = position1;
+
+visited[a + "," + b] = 2;
+
 for (let i = 0; i < arr.length; i++) {
-  output += arr[i];
+  if (i % 2 === 0) {
+    position1 = add(position1, move[arr[i]]);
+
+    const [c, d] = position1;
+
+    if (!visited[c + "," + d]) {
+      visited[c + "," + d] = 1;
+      ++visits;
+      continue;
+    }
+
+    visited[c + "," + d] += 1;
+    continue;
+  }
+
+  position2 = add(position2, move[arr[i]]);
+
+  const [e, f] = position2;
+
+  if (!visited[e + "," + f]) {
+    visited[e + "," + f] = 1;
+    ++visits;
+    continue;
+  }
+
+  visited[e + "," + f] += 1;
 }
 
-console.log(output);
+console.log(visits);
