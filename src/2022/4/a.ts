07cfc9fa7b9a63f78e00deee50ac853c6b72f7c7
tadjh <tadjh.brooks@gmail.com>
2022-12-03 23:06:19 -0800

    2022 Day 4

--- src/2022/4/a.ts
+++ src/2022/4/a.ts
@@ -1,17 +1,58 @@
 // deno run --watch --allow-all a.ts
 
 export {};
-const input = await Deno.readTextFile("test.txt");
-const arr = input.split("") as string[];
+const input = await Deno.readTextFile("input.txt");
+const arr = input.split("\r\n").map((el) =>
+  el.split(",").map((a) => {
+    const [s, e] = a.split("-");
+    const range = [];
 
-const _lookup = {
-  "": 0,
-};
+    const start = parseInt(s);
+    const end = parseInt(e);
 
-let output = "";
+    for (let i = start; i < end + 1; i++) {
+      range.push(i);
+    }
+    return range;
+  })
+) as number[][][];
+
+// console.log(arr);
+
+let output = 0;
+
+// const _lookup = {
+//   "": 0,
+// };
 
 for (let i = 0; i < arr.length; i++) {
-  output += arr[i];
+  const [range1, range2] = arr[i];
+
+  if (range1.length > range2.length) {
+    //
+
+    for (let j = 0; j < range2.length; j++) {
+      const res = range1.includes(range2[j]);
+
+      if (!res) break;
+
+      if (range2.length - 1 === j) output++;
+    }
+
+    continue;
+  }
+
+  for (let j = 0; j < range1.length; j++) {
+    const res = range2.includes(range1[j]);
+
+    if (!res) break;
+
+    if (range1.length - 1 === j) output++;
+  }
+
+  continue;
+
+  // output += arr[i];
 }
 
 console.log(output);
--- src/2022/4/b.ts
+++ src/2022/4/b.ts
@@ -1,17 +1,54 @@
 // deno run --watch --allow-all a.ts
 
 export {};
-const input = await Deno.readTextFile("test.txt");
-const arr = input.split("") as string[];
+const input = await Deno.readTextFile("input.txt");
+const arr = input.split("\r\n").map((el) =>
+  el.split(",").map((a) => {
+    const [s, e] = a.split("-");
+    const range = [];
 
-const _lookup = {
-  "": 0,
-};
+    const start = parseInt(s);
+    const end = parseInt(e);
 
-let output = "";
+    for (let i = start; i < end + 1; i++) {
+      range.push(i);
+    }
+    return range;
+  })
+) as number[][][];
+
+// console.log(arr);
+
+let output = 0;
+
+// const _lookup = {
+//   "": 0,
+// };
 
 for (let i = 0; i < arr.length; i++) {
-  output += arr[i];
+  const [range1, range2] = arr[i];
+
+  if (range1.length > range2.length) {
+    //
+
+    for (let j = 0; j < range2.length; j++) {
+      if (range1.includes(range2[j])) {
+        output++;
+        break;
+      }
+    }
+
+    continue;
+  }
+
+  for (let j = 0; j < range1.length; j++) {
+    if (range2.includes(range1[j])) {
+      output++;
+      break;
+    }
+  }
+
+  continue;
 }
 
 console.log(output);
