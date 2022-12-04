52844cf8167bf130f7436eee7f06f8dbd55242e2
tadjh <tadjh.brooks@gmail.com>
2022-12-03 23:59:40 -0800

    2015 day 4

---
+++ src/2015/4/a.ts
@@ -0,0 +1,24 @@
+// deno run --watch --allow-all a.ts
+import {
+  crypto,
+  toHashString,
+} from "https://deno.land/std@0.167.0/crypto/mod.ts";
+
+export {};
+const input = await Deno.readTextFile("input.txt");
+
+let i = 0;
+while (true) {
+  const hash = await crypto.subtle.digest(
+    "MD5",
+    new TextEncoder().encode(input + i)
+  );
+
+  const a = toHashString(hash);
+
+  if (a.startsWith("00000", 0)) {
+    console.log(a, i);
+    break;
+  }
+  i++;
+}
