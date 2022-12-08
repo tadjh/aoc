7c4d18b52148b676f4a93477ac00a9e59cbc4f20
tadjh <tadjh.brooks@gmail.com>
2022-12-07 23:20:04 -0800

    2022 day 8

---
+++ src/2022/8/a.ts
@@ -0,0 +1,88 @@
+// deno run --watch --allow-all a.ts
+
+export {};
+const input = await Deno.readTextFile("input.txt");
+const lines = input.split("\r\n").map((el) => el.split("")) as string[][];
+
+// console.log(lines);
+
+// const result: boolean[][] = [];
+
+function searchUp(target: number, y: number, x: number): boolean {
+  if (y - 1 === -1) return true;
+  if (parseInt(lines[y - 1][x]) < target) return searchUp(target, y - 1, x);
+  return false;
+}
+
+function searchRight(target: number, y: number, x: number): boolean {
+  if (x + 1 === lines.length) return true;
+  if (parseInt(lines[y][x + 1]) < target) return searchRight(target, y, x + 1);
+  return false;
+}
+
+function searchDown(target: number, y: number, x: number): boolean {
+  if (y + 1 === lines.length) return true;
+  if (parseInt(lines[y + 1][x]) < target) return searchDown(target, y + 1, x);
+  return false;
+}
+
+function searchLeft(target: number, y: number, x: number): boolean {
+  if (x - 1 === -1) return true;
+  if (parseInt(lines[y][x - 1]) < target) return searchLeft(target, y, x - 1);
+  return false;
+}
+
+let score = 0;
+
+function isVisible(row = 0, col = 0) {
+  for (let i = row; i < lines.length; i++) {
+    if (i === 0 || i === lines.length - 1) {
+      score += 1;
+      return true;
+    }
+    for (let j = col; j < lines.length; j++) {
+      if (j === 0 || j === lines.length - 1) {
+        score += 1;
+        return true;
+      }
+      const current = parseInt(lines[i][j]);
+
+      const up = searchUp(current, i, j);
+
+      if (up) {
+        score += 1;
+        return true;
+      }
+
+      const right = searchRight(current, i, j);
+
+      if (right) {
+        score += 1;
+        return true;
+      }
+
+      const down = searchDown(current, i, j);
+
+      if (down) {
+        score += 1;
+        return true;
+      }
+
+      const left = searchLeft(current, i, j);
+
+      if (left) {
+        score += 1;
+        return true;
+      }
+
+      return false;
+    }
+  }
+  return false;
+}
+
+const output = lines.map((row, rowIndex) =>
+  row.map((_col, colIndex) => isVisible(rowIndex, colIndex))
+);
+
+console.log(score);
---
+++ src/2022/8/b.ts
@@ -0,0 +1,59 @@
+// deno run --watch --allow-all a.ts
+
+export {};
+const input = await Deno.readTextFile("input.txt");
+const lines = input.split("\r\n").map((el) => el.split("")) as string[][];
+
+// console.log(lines);
+
+function searchUp(target: number, y: number, x: number, score = 0): number {
+  if (y - 1 === -1) return score;
+  if (parseInt(lines[y - 1][x]) < target)
+    return searchUp(target, y - 1, x, score + 1);
+  return score + 1;
+}
+
+function searchRight(target: number, y: number, x: number, score = 0): number {
+  if (x + 1 === lines.length) return score;
+  if (parseInt(lines[y][x + 1]) < target)
+    return searchRight(target, y, x + 1, score + 1);
+  return score + 1;
+}
+
+function searchDown(target: number, y: number, x: number, score = 0): number {
+  if (y + 1 === lines.length) return score;
+  if (parseInt(lines[y + 1][x]) < target)
+    return searchDown(target, y + 1, x, score + 1);
+  return score + 1;
+}
+
+function searchLeft(target: number, y: number, x: number, score = 0): number {
+  if (x - 1 === -1) return score;
+  if (parseInt(lines[y][x - 1]) < target)
+    return searchLeft(target, y, x - 1, score + 1);
+  return score + 1;
+}
+
+let score = 0;
+
+function visibilityScore(row = 0, col = 0): number {
+  for (let i = row; i < lines.length; i++) {
+    for (let j = col; j < lines.length; j++) {
+      const current = parseInt(lines[i][j]);
+      const up = searchUp(current, i, j);
+      const right = searchRight(current, i, j);
+      const down = searchDown(current, i, j);
+      const left = searchLeft(current, i, j);
+      const value = up * right * down * left;
+      score = Math.max(value, score);
+      return value;
+    }
+  }
+  return 0;
+}
+
+const output = lines.map((row, rowIndex) =>
+  row.map((_col, colIndex) => visibilityScore(rowIndex, colIndex))
+);
+
+console.log(score);
