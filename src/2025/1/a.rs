da2a919abaf68b85d3893c5472806d61fff3a4dc
tadjh <hello@tadjh.com>
2025-12-11 10:53:42 -0800

    Read and split file contents in a.rs and b.rs
    Both a.rs and b.rs now read 'test.txt' and split its contents into lines, printing the resulting vector. The unused FILE_NAME constant was removed from a.rs.

--- src/2025/1/a.rs
+++ src/2025/1/a.rs
@@ -1,8 +1,7 @@
 use std::fs;
 
-const FILE_NAME: &str = "test.txt";
-
 fn main() {
-    let contents = fs::read_to_string(FILE_NAME).expect("Should have been able to read the file");
-    println!("{contents}");
+    let contents = fs::read_to_string("test.txt").expect("Should have been able to read the file");
+    let lines: Vec<&str> = contents.split("\n").collect();
+    println!("{:?}", lines);
 }
--- src/2025/1/b.rs
+++ src/2025/1/b.rs
@@ -1,3 +1,7 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let contents = fs::read_to_string("test.txt").expect("Should have been able to read the file");
+    let lines: Vec<&str> = contents.split("\n").collect();
+    println!("{:?}", lines);
 }
