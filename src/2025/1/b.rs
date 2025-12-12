2ee8a39db344aaafbc3adf739ecfe1ec2cf3c398
tadjh <hello@tadjh.com>
2025-12-11 19:42:20 -0800

    giving up for now on part b

--- src/2025/1/b.rs
+++ src/2025/1/b.rs
@@ -3,25 +3,68 @@ use std::fs;
 fn main() {
     let contents = fs::read_to_string("input.txt").expect("Should have been able to read the file");
 
-    let mut value: i32 = 50;
-    let mut zeroes: i32 = 0;
+    let mut curr: i32 = 50;
+    let mut acc: i32 = 0;
 
-    println!("The dial starts by pointing at {value}");
+    println!("The dial starts by pointing at {curr}");
 
     for line in contents.lines() {
-        let moves: i32 = line[1..]
-            .parse()
-            .expect("Should have been a valid integer.");
-        let result = match &line[0..1] {
-            "L" => moves * -1,
-            "R" => moves,
-            _ => panic!("The dial was not rotated."),
-        };
-        value = (value + result).rem_euclid(100);
-        if value == 0 {
-            zeroes += 1;
-        }
-        println!("The dial is rotated {line} to point at {value}.");
+        let parsed = parse_line(curr, acc, line);
+        curr = parsed.current;
+        acc = parsed.accumulator;
     }
-    println!("{:?}", zeroes);
+    println!("{:?}", acc);
+}
+
+struct Parsed {
+    current: i32,
+    accumulator: i32,
+}
+
+fn parse_line(current: i32, zeroes: i32, line: &str) -> Parsed {
+    let direction = &line[0..1];
+    let distance = &line[1..]
+        .parse()
+        .expect("Should have been a valid integer.");
+    let value = match direction {
+        "L" => current - distance,
+        "R" => current + distance,
+        _ => panic!("The dial was not rotated."),
+    };
+
+    let mut quotient = 0;
+    let mut accumulator = 0;
+
+    if current == 0 && value < 0 {
+        quotient = value.abs() / 100;
+        accumulator += quotient;
+    } else if value < 0 {
+        quotient = (value.abs() / 100) + 1;
+        accumulator += quotient;
+    } else if value > 100 {
+        quotient = value / 100;
+        accumulator += quotient;
+    }
+
+    let next = value.rem_euclid(100);
+
+    if next == 0 {
+        accumulator += 1;
+    }
+
+    // println!(
+    //     "value: {value}, distance: {distance}, quotient: {quotient}, accumulator: {accumulator}"
+    // );
+
+    if quotient > 0 {
+        println!(
+            "The dial is rotated {line} to point at {next}; during this rotation, it points at zero {quotient} times."
+        );
+    } else {
+        println!("The dial is rotated {line} to point at {next}.");
+    }
+    Parsed {
+        current: next,
+        accumulator: accumulator + zeroes,
+    }
 }
