2c6e333cc8c0ccae7123a8b96125883a70cbeb7d
TadjhBrooks <tadjh.brooks@gmail.com>
2025-12-13 09:56:12 -0800

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,16 @@
 pub fn get_filename() -> String {
     let args: Vec<String> = std::env::args().collect();
-    if args.iter().any(|arg| arg == "-i") {
+
+    if args.iter().any(|arg| arg == "-t") {
         return String::from("input.txt");
     }
 
-    if args.iter().any(|arg| arg == "-t") {
-        return String::from("test.txt");
-    }
-
-    return String::from("test.txt");
+    String::from("test.txt")
 }
 
 pub fn get_input() -> String {
     let filename = get_filename();
-    std::fs::read_to_string(filename).expect("Should have been able to read the file")
+    std::fs::read_to_string(filename).expect("Unable to read the file")
 }
 
 pub fn update_input(content: &str) {
